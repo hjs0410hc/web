8fa792ed051c0572b97df8891765708b088998b4
hjs0410hc <hjs0410hc@github.com>
2022-05-10 23:57:15 +0900

    SignIn&SignUp Implemented..

--- src/App.js
+++ src/App.js
@@ -5,17 +5,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GlobalStyle from './components/GlobalStyle';
 import Pages from '/pages';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-
+import { ApolloClient, ApolloProvider, InMemoryCache,createHttpLink } from '@apollo/client';
+import { setContext } from 'apollo-link-context';
 const uri = process.env.API_URI;
+const httpLink = createHttpLink({uri});
 const cache = new InMemoryCache();
 
+const authLink = setContext((_,{headers})=>{
+    return {
+        headers: {
+            ...headers,
+            authorization: localStorage.getItem('token') || ''
+        }
+    }
+})
 
 const client = new ApolloClient({
-    uri,cache,connectToDevTools:true
+    link: authLink.concat(httpLink),
+    cache,resolvers:{},connectToDevTools:true
 });
 
+
 const App = ()=>{
+    const data ={
+        isLoggedIn: !!localStorage.getItem('token')
+    }
+    cache.writeData({data})
+    client.onResetStore(()=>cache.writeData({data})
+    )
     return(
         <ApolloProvider client={client}>
             <GlobalStyle />
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,14 +1,64 @@
-import React, {useEffect} from 'react';
+import React, {useEffect,useState} from 'react';
+import styled from 'styled-components';
+import Button from '../components/Button';
+import { useMutation, useApolloClient, gql } from '@apollo/client';
+import UserForm from '../components/UserForm';
+
+const Wrapper= styled.div`
+    border:1px solid $f5f4f0;
+    max-width: 500px;
+    padding: 1em;
+    margin: 0 auto;
+
+
+`
+
+const Form = styled.form`
+    label,input{
+        display:block;
+        line-height:2em;
+    }
+
+`
+const SIGNUP_USER = gql`
+    mutation signUp($email: String!, $username: String!, $password:String!){
+        signUp(email:$email,username:$username,password:$password)
+    }
+
+`
+
+
 
 const SignUp = props=>{
+    const client = useApolloClient();
+
+
+    const [signUp, {loading,error}] = useMutation(SIGNUP_USER,{
+        onCompleted: data =>{
+            localStorage.setItem('token',data.signUp);
+            client.writeData({data:{isLoggedIn:true}})
+            props.history.push('/');
+        }
+    })
+    const [values,setValues] = useState();
+
+    const onChange = event=>{
+        setValues({
+            ...values,
+            [event.target.name]: event.target.value
+        })
+    };
+
     useEffect(()=>{ // this will work ONCE.
         document.title = 'Sign Up - Notedly';
     },[])
 
     return (
-        <div>
-            <p>Sign Up</p>
-        </div>
+        <React.Fragment>
+            <UserForm action={signUp} formType="signup" />
+            {loading && <p>Loading...</p>}
+            {error && <p>Error creating an account!</p>}
+        </React.Fragment>
     )
 }
 export default SignUp;
